5a4267c4f05f07867e4505256a1fd450f3eec0ea
Harsh Arora <harsh.arora@citrix.com>
2020-08-11 16:52:18 +0530

    updated exporter version

--- docs/how-to/hpa/values.sh
+++ docs/how-to/hpa/values.sh
@@ -1,6 +1,6 @@
 VPX_IP="VPX_IP"
 CPX_IMAGE="quay.io/citrix/citrix-k8s-cpx-ingress:13.0-47.103"
 CIC_IMAGE="quay.io/citrix/citrix-k8s-ingress-controller:1.6.1"
-EXPORTER="quay.io/citrix/citrix-adc-metrics-exporter:1.4.4"
+EXPORTER="quay.io/citrix/citrix-adc-metrics-exporter:1.4.5"
 VPX_PASSWORD="VPX_PASSWORD"
 VPX_VIP="VIRTUAL_IP_VPX"
