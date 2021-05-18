a8c347ee551a99b0b356f2a206651aa38a00906f
root <priyanka.sharma@citrix.com>
2021-05-18 09:59:58 +0530

    Updating SML script

--- docs/how-to/sml/smlite.py
+++ docs/how-to/sml/smlite.py
@@ -13,13 +13,15 @@ import manifestCreator
 import logging
 import warnings
 import argparse
+import subprocess
+from ipaddress import ip_address, IPv4Address
 from collections import OrderedDict
 from kubernetes import client, config
 from itertools import chain, repeat
 from pathlib import Path
 
 __author__ = "Priyanka"
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __maintainer__ = "Priyanka"
 __email__ = "priyanka.sharma@citrix.com"
 
@@ -38,8 +40,11 @@ warnings.filterwarnings('ignore', message='Unverified HTTPS request')
 
 ports_used = []
 cpx_count = 1
-ingress_class = "tier-2-cpx"
-cpx_ingress_name = "cpx-ingress"
+cpx_ingress_class = "tier-2-cpx"
+vpx_ingress_class = "tier-1-vpx"
+tier1_ingress_name = "tier1-vpx-ingress"
+frontend_cpx_count = 1
+chart_name = "sml"
 output_yaml = "smlite-all-in-one.yaml"
 
 
@@ -92,7 +97,10 @@ def main():
 
     app_frontend_svc_name = input("Please provide name of the service exposed to tier-1: ")
     app_hostname = input("Please provide hostname for exposing \""+ app_frontend_svc_name +"\" service: ")
-    
+
+    if not namespace:
+        namespace = "default"
+
     for svc in svcs:
         if(".yaml" in svc):
             with open(svc, 'r') as stream:
@@ -118,17 +126,118 @@ def main():
 #                            ('spec',service['spec']),
             converted_yamls_dict_list.extend(convert_yaml_to_sml(service,app_frontend_svc_name,app_hostname))
 
+    helm_ver = subprocess.run(["helm version"],shell=True,capture_output=True).stdout.decode('utf-8')
+    matchObj = re.search(r'Version:"v3.*', helm_ver, re.M|re.I)
+    if not matchObj:
+        logger.error("Couldn't find Helm with version 3.x and above.")
+        sys.exit("Please install Helm 3.x and put it in the PATH. This https://github.com/citrix/citrix-helm-charts/blob/master/Helm_Installation_version_3.md can be followed for the same.")
+
+    subprocess.run(["helm repo add citrix https://citrix.github.io/citrix-helm-charts/"],shell=True,capture_output=True)
+    
+
+    '''
+    ADM details
+    '''
+    prompts = chain(['Citrix ADM required? (Y/N): '], repeat("Invaild input. Please respond with (Y/N): "))
+    adm_required = validate_input(prompts,"yesORno")
+    if adm_required.lower() == "y":
+        prompts = chain(['Please provide IP of ADM Agent(svcIP of container agent) for Citrix ADC CPX: '], repeat("Invaild IP. Please enter and IPv4 IP: "))
+        cpx_adm_agent_ip = validate_input(prompts,"ip")
+        adm_secret = input("Please provide name of K8s Secret created using ADM Agent credentials. Press ENTER for 'admlogin': ")
+        if not adm_secret:
+            adm_secret = "admlogin"
+
+
     '''
     Adding CPX yamls
     '''
-    rbac = manifestCreator.rbac()
-    rbac = rbac.createRbac()
-    converted_yamls_dict_list.extend(rbac)
     for i in range(1,cpx_count+1):
-        cpx_cic_yaml = manifestCreator.cpxCic({"name":cpx_ingress_name+str(i),"ingressClass":ingress_class+str(i)})
-        cpx_cic_yaml = cpx_cic_yaml.create()
-        converted_yamls_dict_list.extend(cpx_cic_yaml)
+        if adm_required.lower() == "y":
+            output = subprocess.run(["helm template "+ chart_name + str(i) + " citrix/citrix-cloud-native --set cpx.enabled=true,cpx.license.accept=yes,cpx.ingressClass[0]="+cpx_ingress_class+str(i)+",cpx.coeConfig.required=true,cpx.coeConfig.timeseries.metrics.enable=true,cpx.coeConfig.distributedTracing.enable=true,cpx.coeConfig.endpoint.server="+ cpx_adm_agent_ip +",cpx.ADMSettings.ADMIP=" + cpx_adm_agent_ip +",cpx.ADMSettings.loginSecret=" + adm_secret + " -n "+namespace+" > temp.yaml"],shell=True,capture_output=True)
+#            output = subprocess.run(["helm template "+ chart_name + str(i) + " citrix/citrix-cloud-native --set cpx.enabled=true,cpx.license.accept=yes,cpx.ingressClass[0]="+cpx_ingress_class+str(i)+",cpx.coeConfig.required=true,cpx.coeConfig.timeseries.metrics.enable=true,cpx.coeConfig.distributedTracing.enable=true,cpx.coeConfig.endpoint.server="+ cpx_adm_agent_ip +",cpx.ADMSettings.ADMIP=" + cpx_adm_agent_ip +",cpx.ADMSettings.loginSecret=" + adm_secret + " -n "+namespace+" > temp.yaml"],shell=True,capture_output=True)
+            if output.stderr:
+                logger.error("Helm chart command failed with error {}" .format(output.stderr))
+                sys.exit("Please ensure you have provided proper inputs.")
+        else:
+            output = subprocess.run(["helm template "+ chart_name + str(i) + " citrix/citrix-cloud-native --set cpx.enabled=true,cpx.license.accept=yes,cpx.ingressClass[0]="+cpx_ingress_class+str(i)+ " -n "+namespace+" > temp.yaml"],shell=True,capture_output=True)
+            if output.stderr:
+                logger.error("Helm chart command failed with error {}" .format(output.stderr))
+                sys.exit("Please ensure you have provided proper inputs.")
+        
+        with open('temp.yaml', 'r') as stream:
+            yaml_dictionaries = list(yaml.safe_load_all(stream))
+        if i > 1:
+            list_to_remove = []
+            for yaml_instance in yaml_dictionaries:
+                if yaml_instance['kind'] == "ServiceAccount" or yaml_instance['kind'] == "ClusterRole" or yaml_instance['kind'] == "ClusterRoleBinding":
+                    list_to_remove.append(yaml_instance)
+                elif yaml_instance['kind'] == "Deployment":
+                    yaml_instance['spec']['template']['spec']['serviceAccountName'] = re.sub("sml"+str(i),"sml1",yaml_instance['spec']['template']['spec']['serviceAccountName'])
+            yaml_dictionaries = [x for x in yaml_dictionaries if x not in list_to_remove]
+            converted_yamls_dict_list.extend(yaml_dictionaries)
+        else:
+            converted_yamls_dict_list.extend(yaml_dictionaries)
+#    rbac = manifestCreator.rbac()
+#    rbac = rbac.createRbac()
+#    converted_yamls_dict_list.extend(rbac)
+#        cpx_cic_yaml = manifestCreator.cpxCic({"name":chart_name+str(i),"ingressClass":cpx_ingress_class+str(i)})
+#        cpx_cic_yaml = cpx_cic_yaml.create()
+#        converted_yamls_dict_list.extend(cpx_cic_yaml)
 
+    prompts = chain(['Citrix Ingress Controller for tier-1 ADC required? (Y/N): '], repeat("Invaild input. Please respond with (Y/N): "))
+    create_cic = validate_input(prompts,"yesORno")
+    if create_cic.lower() == "y":
+        prompts = chain(['Please provide tier-1 ADC NSIP: '], repeat("Invaild IP. Please enter and IPv4 IP: "))
+        ns_ip = validate_input(prompts,"ip")
+        prompts = chain(['Please provide tier-1 ADC VIP: '], repeat("Invaild IP. Please enter and IPv4 IP: "))
+        ns_vip = validate_input(prompts,"ip")
+        ns_secret = input("Please provide name of K8s Secret created using ADC credentials. Press ENTER for 'nslogin': ")
+        if not ns_secret:
+            ns_secret = "nslogin"
+        if adm_required.lower() == "y":
+            prompts = chain(['Please provide IP of ADM Agent(podIP of container agent) for Citrix ADC VPX/MPX: '], repeat("Invaild IP. Please enter and IPv4 IP: "))
+            vpx_adm_agent_ip = validate_input(prompts,"ip")
+
+        app_frontend_svc_port = input("Please provide port used to expose CPX service to Tier-1 ADC: ")
+        prompts = chain(["Please provide protocol used to expose CPX service to Tier-1 ADC (tcp/udp/http/https/grpc): "], repeat("Invaild protocol. Please choose a protocol from (tcp/udp/http/https/grpc): "))
+        app_frontend_svc_protocol = validate_input(prompts,"protocol")
+
+        if app_frontend_svc_protocol == "grpc":
+            app_frontend_svc_protocol = "tcp"
+
+        if app_frontend_svc_protocol == "https":
+            tls = input("Please give secret-name for TLS certificate for \""+ app_frontend_svc_name +"\" svc: ")
+        else:
+            tls = ""
+
+
+        if adm_required.lower() == "y":
+            output = subprocess.run(["helm template "+ chart_name + " citrix/citrix-cloud-native --set cic.enabled=true,cic.nsIP="+ ns_ip +",cic.nsVIP="+ ns_vip +",cic.license.accept=yes,cic.adcCredentialSecret="+ ns_secret +",cic.ingressClass[0]="+vpx_ingress_class+",cic.coeConfig.required=true,cic.coeConfig.timeseries.metrics.enable=true,cic.coeConfig.timeseries.port=5563,cic.coeConfig.distributedTracing.enable=true,cic.coeConfig.transactions.enable=true,cic.coeConfig.transactions.port=5557,cic.coeConfig.endpoint.server="+ vpx_adm_agent_ip +" -n "+ namespace +" > temp.yaml"],shell=True,capture_output=True)
+            if output.stderr:
+                logger.error("Helm chart command failed with error {}" .format(output.stderr))
+                sys.exit("Please ensure you have provided proper inputs.")
+
+            ingress_yaml = manifestCreator.ingress({"name":tier1_ingress_name,"protocol":app_frontend_svc_protocol.lower(),
+                                                    "ingressClass":vpx_ingress_class,"tls":tls,"admRequired":True,
+                                                    "serviceDetails":{app_hostname:[{"serviceName":"sml"+str(frontend_cpx_count)+"-cpx-service",
+                                                                                     "servicePort":app_frontend_svc_port}]}})
+        else:
+            output = subprocess.run(["helm template "+ chart_name + " citrix/citrix-cloud-native --set cic.enabled=true,cic.nsIP="+ ns_ip +",cic.license.accept=yes,cic.adcCredentialSecret="+ ns_secret +",cic.ingressClass[0]="+vpx_ingress_class+" -n "+ namespace +" > temp.yaml"],shell=True,capture_output=True)
+            if output.stderr:
+                logger.error("Helm chart command failed with error {}" .format(output.stderr))
+                sys.exit("Please ensure you have provided proper inputs.")
+
+            ingress_yaml = manifestCreator.ingress({"name":tier1_ingress_name,"protocol":app_frontend_svc_protocol.lower(),
+                                                    "ingressClass":vpx_ingress_class,"tls":tls,
+                                                    "serviceDetails":{app_hostname:[{"serviceName":"sml"+str(frontend_cpx_count)+"-cpx-service",
+                                                                                     "servicePort":app_frontend_svc_port}]}})
+        with open('temp.yaml', 'r') as stream:
+            yaml_dictionaries = list(yaml.safe_load_all(stream))
+        converted_yamls_dict_list.extend(yaml_dictionaries)
+
+        ingress_yaml = ingress_yaml.create()
+        converted_yamls_dict_list.append(ingress_yaml)
+    
     '''
     Writing dictionay of YAML into a .yaml file
     '''
@@ -157,6 +266,15 @@ def validate_input(prompts, input_type, port_list=None):
     elif input_type == "apiServerIPPort":
         ip_port_re = re.compile(r'^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]):[0-9]+$')
         valid_response = next(filter(ip_port_re.match, replies))
+    elif input_type == "ip":
+        replies = map(str, replies)
+        def check_ip(ip):
+            try:
+                return True if type(ip_address(ip)) is IPv4Address else False
+            except ValueError:
+                return False 
+        valid_response = next(filter(check_ip, replies))
+        valid_response = str(valid_response)
     return valid_response
 
 '''
@@ -182,8 +300,8 @@ def create_smlite_yamls(yaml_instance,frontend,hostname):
 
     global ports_used
     global cpx_count
-    global ingress_class
-    global cpx_ingress_name
+    global cpx_ingress_class
+    global chart_name
     headless_svc_tag = "-headless"
     ew_ing_tag = "-ingress"
     ns_ing_tag = "-ns-ingress"
@@ -236,7 +354,7 @@ def create_smlite_yamls(yaml_instance,frontend,hostname):
         '''
         Creating service for appication so that FQDN of the microservice points to Citrix ADC CPX IP address
         '''
-        service_yaml = manifestCreator.service({"name":service_name,"appLabel":cpx_ingress_name+str(cpx_count),"ports":ports})
+        service_yaml = manifestCreator.service({"name":service_name,"appLabel":chart_name+str(cpx_count)+"-cpx","ports":ports})
         service_yaml = service_yaml.create()
         list_of_sml_yamls.append(service_yaml)
 
@@ -244,27 +362,40 @@ def create_smlite_yamls(yaml_instance,frontend,hostname):
         Creating ingress for each microservice so they get configured in CPX
         '''
         if (protocol.lower() == "tcp" or protocol.lower() == "udp"):
+#            if "targetPort" in port_details:
+#                ingress_yaml = manifestCreator.ingress({"name":service_name+ew_ing_tag,"protocol":protocol.lower(),
+#                                                    "ingressClass":cpx_ingress_class+str(cpx_count),"tls":tls,
+#                                                    "serviceDetails":{"serviceName":service_name+headless_svc_tag,
+#                                                                      "servicePort":  port_details["targetPort"]}})
+#            else:
             ingress_yaml = manifestCreator.ingress({"name":service_name+ew_ing_tag,"protocol":protocol.lower(),
-                                                    "ingressClass":ingress_class+str(cpx_count),"tls":tls,
+                                                    "ingressClass":cpx_ingress_class+str(cpx_count),"tls":tls,
                                                     "serviceDetails":{"serviceName":service_name+headless_svc_tag,
                                                                       "servicePort":  port_details["port"]}})
             if frontend == service_name:
                 frontend_ingress_yaml = manifestCreator.ingress({"name":service_name+ns_ing_tag,"protocol":protocol.lower(),
-                                                                 "ingressClass":ingress_class+str(cpx_count),"tls":tls,
+                                                                 "ingressClass":cpx_ingress_class+str(cpx_count),"tls":tls,
                                                                  "serviceDetails":{"serviceName":service_name+headless_svc_tag,
                                                                                    "servicePort": port_details["port"]}})
 
         else:
+#            if "targetPort" in port_details:
+#                ingress_yaml = manifestCreator.ingress({"name":service_name+ew_ing_tag,"protocol":protocol.lower(),
+#                                                    "ingressClass":cpx_ingress_class+str(cpx_count),"tls":tls,
+#                                                    "serviceDetails":{service_name:[{"serviceName":service_name+headless_svc_tag,
+#                                                                                     "servicePort":port_details["targetPort"]}]}})
+#            else:
             ingress_yaml = manifestCreator.ingress({"name":service_name+ew_ing_tag,"protocol":protocol.lower(),
-                                                    "ingressClass":ingress_class+str(cpx_count),"tls":tls,
+                                                    "ingressClass":cpx_ingress_class+str(cpx_count),"tls":tls,
                                                     "serviceDetails":{service_name:[{"serviceName":service_name+headless_svc_tag,
                                                                                      "servicePort":port_details["port"]}]}})
 #                                                                      service_name+"."+namespace+request_other_namespace:[{
 #                                                                                     "serviceName":service_name+headless_svc_tag,
 #                                                                                     "servicePort":port_details["port"]}]}})
             if frontend == service_name:
+                frontend_cpx_count = str(cpx_count)
                 frontend_ingress_yaml = manifestCreator.ingress({"name":service_name+ns_ing_tag,"protocol":protocol.lower(),
-                                                                 "ingressClass":ingress_class+str(cpx_count),"tls":tls,
+                                                                 "ingressClass":cpx_ingress_class+str(cpx_count),"tls":tls,
                                                                  "serviceDetails":{hostname:[{"serviceName":service_name+headless_svc_tag,
                                                                                               "servicePort":port_details["port"]}]}})
 
@@ -378,6 +509,7 @@ def write_dictionaries_into_yaml(yaml_dict,output_file):
                                 continue
                             y["metadata"]["annotations"][k] = QuotedString(v)
             stream_write.write(yaml.dump_all(yaml_dict, default_flow_style=False, sort_keys=False))
+            logger.info("Please note Tier-1 ADC VPX ingress "+tier1_ingress_name+" is created with basic config. Please edit it as per your requirements")
             logger.info("ServiceMesh Lite YAMLs are created and is present in \"smlite-all-in-one.yaml\" file.")
 
 class QuotedString(str):  # just subclass the built-in str
