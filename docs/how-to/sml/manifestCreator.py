a12fbd2216721a8c16869bc3f54e87e59fdb7967
Subash Dangol <subash.dangol@citrix.com>
2023-03-15 18:47:50 +0530

    Releasing CIC v1.30.1
    Signed-off-by: Subash Dangol <subash.dangol@citrix.com>

--- docs/how-to/sml/manifestCreator.py
+++ docs/how-to/sml/manifestCreator.py
@@ -148,7 +148,7 @@ class cpxCic:
         self.cicContainerName = "cic"
         self.cpxContainerName = "cpx"
         self.cpxImage = "quay.io/citrix/citrix-k8s-cpx-ingress:13.0-83.27"
-        self.cicImage = "quay.io/citrix/citrix-k8s-ingress-controller:1.29.5"
+        self.cicImage = "quay.io/citrix/citrix-k8s-ingress-controller:1.30.1"
         self.imagePullPolicy = "Always"
         self.readinessProbe = True
         self.serviceAccountName = "citrix"
