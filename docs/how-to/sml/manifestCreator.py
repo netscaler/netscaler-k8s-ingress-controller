5141b6a9a51bce2fdd5cc2acb8b36dad59d7d973
Apoorva Kamath <apoorva.kamath@citrix.com>
2023-04-04 12:01:21 +0530

    CIC Release 1.31.3

--- docs/how-to/sml/manifestCreator.py
+++ docs/how-to/sml/manifestCreator.py
@@ -148,7 +148,7 @@ class cpxCic:
         self.cicContainerName = "cic"
         self.cpxContainerName = "cpx"
         self.cpxImage = "quay.io/citrix/citrix-k8s-cpx-ingress:13.0-83.27"
-        self.cicImage = "quay.io/citrix/citrix-k8s-ingress-controller:1.30.1"
+        self.cicImage = "quay.io/citrix/citrix-k8s-ingress-controller:1.31.3"
         self.imagePullPolicy = "Always"
         self.readinessProbe = True
         self.serviceAccountName = "citrix"
