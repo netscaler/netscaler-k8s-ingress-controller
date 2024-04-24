d6b92fc75fd6a36d4fa32c88e6a3ef2768249f1d
Arijit Ray <arijit.ray@citrix.com>
2024-04-24 21:23:58 +0530

    Releasing NSIC "1.41.5"

--- example/hpa-demo/values.sh
+++ example/hpa-demo/values.sh
@@ -1,6 +1,6 @@
 VPX_IP="VPX_IP"
-CPX_IMAGE="quay.io/netscaler/netscaler-cpx:14.1-17.38"
-CIC_IMAGE="quay.io/netscaler/netscaler-k8s-ingress-controller:1.40.12"
+CPX_IMAGE="quay.io/netscaler/netscaler-cpx:14.1-17.101"
+CIC_IMAGE="quay.io/netscaler/netscaler-k8s-ingress-controller:1.41.5"
 EXPORTER="quay.io/citrix/citrix-adc-metrics-exporter:1.4.9"
 VPX_PASSWORD="VPX_PASSWORD"
 VPX_VIP="VIRTUAL_IP_VPX"
